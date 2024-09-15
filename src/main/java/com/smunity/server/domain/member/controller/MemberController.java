10a8b7e59f778c0a84c09dfe3a9f58ac9f0f52eb
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 20:15:29 +0900

    feat: 사용자 탈퇴 API (#14)

--- src/main/java/com/smunity/server/domain/member/controller/MemberController.java
+++ src/main/java/com/smunity/server/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.smunity.server.domain.member.controller;
 
 import com.smunity.server.domain.member.dto.MemberInfoResponseDto;
+import com.smunity.server.domain.member.service.MemberCommandService;
 import com.smunity.server.domain.member.service.MemberQueryService;
 import com.smunity.server.global.security.annotation.AuthMember;
 import com.smunity.server.global.validation.annotation.PermissionCheck;
@@ -9,10 +10,7 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberQueryService memberQueryService;
+    private final MemberCommandService memberCommandService;
 
     @GetMapping
     public ResponseEntity<Page<MemberInfoResponseDto>> findAll(@ParameterObject Pageable pageable) {
@@ -37,5 +36,11 @@ public class MemberController {
     public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@AuthMember Long memberId) {
         MemberInfoResponseDto responseDto = memberQueryService.findById(memberId);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteMember(@AuthMember Long memberId) {
+        memberCommandService.deleteMember(memberId);
+        return ResponseEntity.noContent().build();
     }
 }
