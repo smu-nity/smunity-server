501d2765af218a2b19e8375e551a1cd739140d5d
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 20:05:45 +0900

    feat: 사용자 정보 조회 API (#13)

--- src/main/java/com/smunity/server/domain/member/controller/MemberController.java
+++ src/main/java/com/smunity/server/domain/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class MemberController {
     public ResponseEntity<Page<MemberInfoResponseDto>> findAll(@ParameterObject Pageable pageable) {
         Page<MemberInfoResponseDto> responseDtoPage = memberQueryService.findAll(pageable);
         return ResponseEntity.ok(responseDtoPage);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberInfoResponseDto> findById(@PathVariable Long id) {
+        MemberInfoResponseDto responseDto = memberQueryService.findById(id);
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/me")
