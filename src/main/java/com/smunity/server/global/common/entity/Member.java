6780dbf9e394f2cf334879991a934da7954ef572
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 17:21:55 +0900

    feat: Member 엔티티 (#5)

---
+++ src/main/java/com/smunity/server/global/common/entity/Member.java
@@ -0,0 +1,28 @@
+package com.smunity.server.global.common.entity;
+
+import com.smunity.server.global.common.entity.enums.MemberRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private MemberRole role = MemberRole.ROLE_USER;
+}
