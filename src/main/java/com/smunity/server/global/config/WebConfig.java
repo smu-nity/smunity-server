b2d23f544d73918770221bd2f66803dd14a40544
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 20:02:22 +0900

    feat: 사용자 추출 어노테이션 등록 (#13)

---
+++ src/main/java/com/smunity/server/global/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.smunity.server.global.config;
+
+import com.smunity.server.global.security.annotation.AuthMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Spring MVC 설정을 위한 클래스
+ * WebMvcConfigurer 인터페이스를 구현하여 커스텀 설정을 추가
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
+
+    // 커스텀 Argument Resolver 추가
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authMemberArgumentResolver);
+    }
+}
