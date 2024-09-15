c937e46c72453f5113a0a22e1deb49cd4355ef57
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 16:52:26 +0900

    feat: 프로젝트 생성 (#2)

---
+++ src/main/java/com/smunity/server/SmunityServerApplication.java
@@ -0,0 +1,13 @@
+package com.smunity.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmunityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmunityServerApplication.class, args);
+    }
+
+}
