a3a68505f63a87c240eb6a8d4e0e96fc006b3ccc
Hyunmin Choi <hyunmin-choi@naver.com>
2024-09-15 19:38:46 +0900

    [FEATURE] JWT 인증·인가 구현 (#20)
    * feat: JWT 인증 및 예외 필터 (#9)
    
    * feat: 인증 및 인가 오류 핸들러 추가 (#9)
    
    * refactor: 패키지 구조 변경 (#9)

---
+++ src/main/java/com/smunity/server/global/security/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package com.smunity.server.global.security.handler;
+
+import com.smunity.server.global.exception.code.ErrorCode;
+import com.smunity.server.global.security.exception.handler.JwtAuthenticationExceptionHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+/**
+ * 접근 권한이 없는 요청에 대한 오류 응답을 처리하는 핸들러 클래스
+ * 사용자가 요청한 자원에 대한 접근 권한이 없는 경우
+ */
+@Slf4j
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    /**
+     * 접근 거부 시 호출되는 메서드
+     */
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException ex) throws IOException {
+        JwtAuthenticationExceptionHandler.handleException(response, ex, ErrorCode.FORBIDDEN_EXCEPTION);
+    }
+}
---
+++ src/main/java/com/smunity/server/global/security/handler/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package com.smunity.server.global.security.handler;
+
+import com.smunity.server.global.exception.code.ErrorCode;
+import com.smunity.server.global.security.exception.handler.JwtAuthenticationExceptionHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+/**
+ * JWT 인증 실패 시 오류 응답을 보내는 EntryPoint 클래스
+ * JWT 인증 시 인증 정보가 없거나 잘못된 경우
+ */
+@Slf4j
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * 인증 실패 시 호출되는 메서드
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException ex) throws IOException {
+        JwtAuthenticationExceptionHandler.handleException(response, ex, ErrorCode.UNAUTHORIZED_EXCEPTION);
+    }
+}
