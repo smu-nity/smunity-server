1f98c6022bea4133ecbb7cfa73f78c61ead36d36
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-15 18:50:29 +0900

    feat: 시큐리티 설정 (#6)

---
+++ src/main/java/com/smunity/server/global/security/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.smunity.server.global.security.config;
+
+import com.smunity.server.global.security.encoder.Pbkdf2PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Spring Security 설정을 구성하는 클래스
+ * HTTP 보안 설정, 인증 필터 추가 등 보안 관련 설정을 정의
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    /**
+     * PBKDF2 암호화 방식으로 PasswordEncoder 빈을 생성 (Django 기본 암호화 방식)
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
+
+    /**
+     * Spring Security의 필터 체인 설정 구성
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CSRF 보호 비활성화
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // HTTP 응답 헤더 설정
+        http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        // 경로별 인가 작업
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+}
