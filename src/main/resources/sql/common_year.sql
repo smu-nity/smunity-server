f4da66926fc072a28e8dee3a9a70622c066566a3
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-16 14:51:31 +0900

    feat: 학번 데이터 (#27)

---
+++ src/main/resources/sql/common_year.sql
@@ -0,0 +1,26 @@
+INSERT INTO common_year(year_name)
+VALUES ('졸업생');
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2017', 2017, 15, 45, 36, 3, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2018', 2018, 15, 45, 33, 3, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2019', 2019, 15, 45, 33, 3, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2020', 2020, 15, 45, 33, 4, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2021', 2021, 15, 45, 33, 4, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2022', 2022, 15, 45, 33, 4, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2023', 2023, 15, 60, 33, 4, 130);
+
+INSERT INTO common_year(year_name, year_value, major_i, major_s, culture, culture_cnt, total)
+VALUES ('2024', 2024, 15, 60, 33, 4, 130);
