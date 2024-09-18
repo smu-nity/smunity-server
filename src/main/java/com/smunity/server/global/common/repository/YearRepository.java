0d2433a250e46bf23e43423e1e0f578a31c4d4de
Hyunmin Choi <hyunmin-choi@naver.com>
2024-09-18 17:28:35 +0900

    [FEATURE] 회원 테이블 수정 (#32)
    * feat: 연도 리포지토리 (#29)
    
    * feat: 학번 리포지토리 (#29)
    
    * feat: 회원가입 관련 에러 코드 (#29)
    
    * feat: 회원 엔티티 수정 (#29)
    
    * feat: 회원가입 dto 수정 (#29)
    
    * feat: 회원가입 메서드 수정 (#29)

---
+++ src/main/java/com/smunity/server/global/common/repository/YearRepository.java
@@ -0,0 +1,11 @@
+package com.smunity.server.global.common.repository;
+
+import com.smunity.server.global.common.entity.Year;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface YearRepository extends JpaRepository<Year, Long> {
+
+    Optional<Year> findByName(String name);
+}
