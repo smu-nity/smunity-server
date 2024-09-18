b4b7e2c3f1d773297e05c9dc6b52962df319ce90
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-18 21:50:41 +0900

    feat: 수강과목 조회 API (#34)

---
+++ src/main/java/com/smunity/server/domain/course/service/CourseQueryService.java
@@ -0,0 +1,51 @@
+package com.smunity.server.domain.course.service;
+
+import com.smunity.server.domain.course.dto.CourseResponseDto;
+import com.smunity.server.domain.course.dto.ResultResponseDto;
+import com.smunity.server.domain.course.entity.Course;
+import com.smunity.server.domain.course.entity.Standard;
+import com.smunity.server.domain.course.repository.StandardRepository;
+import com.smunity.server.domain.course.repository.course.CourseRepository;
+import com.smunity.server.global.common.entity.Member;
+import com.smunity.server.global.common.entity.Year;
+import com.smunity.server.global.common.entity.enums.Category;
+import com.smunity.server.global.common.repository.MemberRepository;
+import com.smunity.server.global.exception.GeneralException;
+import com.smunity.server.global.exception.code.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CourseQueryService {
+
+    private final MemberRepository memberRepository;
+    private final CourseRepository courseRepository;
+    private final StandardRepository standardRepository;
+
+    public ResultResponseDto<CourseResponseDto> getCourses(Long id, Category category) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new GeneralException(ErrorCode.MEMBER_NOT_FOUND));
+        List<Course> courses = courseRepository.findByMemberIdAndCategory(id, category);
+        List<CourseResponseDto> responseDtoList = CourseResponseDto.from(courses);
+        int total = getTotal(member.getYear(), category);
+        int completed = calculateCompleted(courses);
+        return ResultResponseDto.of(total, completed, responseDtoList);
+    }
+
+    private int getTotal(Year year, Category category) {
+        return standardRepository.findByYearAndCategory(year, category)
+                .map(Standard::getTotal)
+                .orElseGet(year::getTotal);
+    }
+
+    private int calculateCompleted(List<Course> courses) {
+        return courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+    }
+}
