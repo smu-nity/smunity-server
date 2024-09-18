bf627aa9bd0c9f7b278dcd266f78d960abaa9d05
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-18 17:26:35 +0900

    feat: 회원가입 dto 수정 (#29)

--- src/main/java/com/smunity/server/domain/account/dto/RegisterRequestDto.java
+++ src/main/java/com/smunity/server/domain/account/dto/RegisterRequestDto.java
@@ -2,21 +2,34 @@ package com.smunity.server.domain.account.dto;
 
 import com.smunity.server.global.common.entity.Member;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequestDto(
-        @NotBlank(message = "아이디 입력은 필수 입니다.")
+        @NotBlank(message = "학번 입력은 필수 입니다.")
+        @Pattern(regexp = "^\\d{9}$", message = "학번은 9자리 숫자 이어야 합니다.")
         String username,
 
         @NotBlank(message = "비밀번호 입력은 필수 입니다.")
-        @Size(min = 8, message = "비밀번호는 최소 8자리 이이어야 합니다.")
-        String password
+        @Size(min = 8, message = "비밀번호는 최소 8자리 이어야 합니다.")
+        String password,
+
+        @NotBlank(message = "이메일 입력은 필수 입니다.")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+        String email,
+
+        @NotBlank(message = "이름 입력은 필수 입니다.")
+        String name,
+
+        @NotBlank(message = "학과 입력은 필수 입니다.")
+        String department
 ) {
 
-    public Member toEntity(String encodedPw) {
+    public Member toEntity() {
         return Member.builder()
                 .username(username)
-                .password(encodedPw)
+                .email(email)
+                .name(name)
                 .build();
     }
 }
