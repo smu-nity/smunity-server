262ad64ab83b13107c5156f22fcc163701d1b094
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-18 17:18:14 +0900

    feat: 회원 엔티티 수정 (#29)

--- src/main/java/com/smunity/server/global/common/entity/Member.java
+++ src/main/java/com/smunity/server/global/common/entity/Member.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Entity
 @Getter
 @Builder
+@Table(name = "common_member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
@@ -22,9 +23,29 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
     @Builder.Default
     @Enumerated(EnumType.STRING)
     private MemberRole role = MemberRole.ROLE_USER;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "year_id")
+    private Year year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public void setInfo(Year year, Department department, String encodePw) {
+        this.year = year;
+        this.department = department;
+        password = encodePw;
+    }
 
     public void changePassword(String password) {
         this.password = password;
