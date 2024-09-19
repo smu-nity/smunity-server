83f77210535e801fdb9ddf6df6364883d49bde69
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-19 14:36:12 +0900

    feat: 수강과목 학점 조회 API 관련 dto (#36)

---
+++ src/main/java/com/smunity/server/domain/course/dto/CreditResponseDto.java
@@ -0,0 +1,40 @@
+package com.smunity.server.domain.course.dto;
+
+import com.smunity.server.global.common.entity.Member;
+import lombok.Builder;
+
+import static com.smunity.server.domain.course.dto.StatusResponseDto.calculateCompletion;
+import static com.smunity.server.domain.course.dto.StatusResponseDto.calculateRequired;
+import static com.smunity.server.global.common.entity.enums.Category.*;
+
+@Builder
+public record CreditResponseDto(
+        String username,
+        String name,
+        int total,
+        int completed,
+        int major,
+        int culture,
+        int etc,
+        int required,
+        int completion
+) {
+
+    public static CreditResponseDto from(Member member) {
+        int total = member.getYear().getTotal();
+        int completed = member.getCompletedCredits();
+        int major = member.getCompletedCredits(MAJOR_ADVANCED) + member.getCompletedCredits(MAJOR_OPTIONAL);
+        int culture = member.getCompletedCredits(CULTURE);
+        return CreditResponseDto.builder()
+                .username(member.getUsername())
+                .name(member.getName())
+                .total(total)
+                .completed(completed)
+                .major(major)
+                .culture(culture)
+                .etc(completed - major - culture)
+                .required(calculateRequired(total, completed))
+                .completion(calculateCompletion(total, completed))
+                .build();
+    }
+}
