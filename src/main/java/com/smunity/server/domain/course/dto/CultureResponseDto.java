334512995185de251d3ccdb607b51d99018ca046
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-19 17:36:12 +0900

    feat: 교양과목 수강 조회 API dto (#37)

---
+++ src/main/java/com/smunity/server/domain/course/dto/CultureResponseDto.java
@@ -0,0 +1,31 @@
+package com.smunity.server.domain.course.dto;
+
+import com.smunity.server.domain.course.entity.Curriculum;
+import com.smunity.server.global.common.entity.Member;
+import com.smunity.server.global.common.entity.enums.SubDomain;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record CultureResponseDto(
+        SubDomain subDomain,
+        String subDomainName,
+        boolean completed
+) {
+
+    private static CultureResponseDto of(SubDomain subDomain, Member member) {
+        return CultureResponseDto.builder()
+                .subDomain(subDomain)
+                .subDomainName(subDomain.getName())
+                .completed(member.checkCompleted(subDomain))
+                .build();
+    }
+
+    public static List<CultureResponseDto> of(List<Curriculum> curriculums, Member member) {
+        return curriculums.stream()
+                .filter(curriculum -> !curriculum.getSubDomain().equals(member.getSubDomain()))
+                .map(curriculum -> of(curriculum.getSubDomain(), member))
+                .toList();
+    }
+}
