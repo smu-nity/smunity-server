5e60e8f0a5a7999bf1471f5ca0f3a9ed20a488d2
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-22 16:37:20 +0900

    feat: 미수강 전공과목 조회 API (#38)

---
+++ src/main/java/com/smunity/server/domain/major/controller/MajorController.java
@@ -0,0 +1,27 @@
+package com.smunity.server.domain.major.controller;
+
+import com.smunity.server.domain.major.dto.MajorResponseDto;
+import com.smunity.server.domain.major.service.MajorQueryService;
+import com.smunity.server.domain.member.dto.SubjectResponseDto;
+import com.smunity.server.global.common.entity.enums.Category;
+import com.smunity.server.global.security.annotation.AuthMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/majors")
+public class MajorController {
+
+    private final MajorQueryService majorQueryService;
+
+    @GetMapping
+    public ResponseEntity<SubjectResponseDto<MajorResponseDto>> getMajors(@AuthMember Long memberId, @RequestParam(required = false) Category category) {
+        SubjectResponseDto<MajorResponseDto> responseDto = majorQueryService.getMajors(memberId, category);
+        return ResponseEntity.ok(responseDto);
+    }
+}
