fe097e4f21bb3782bcd79fec5a4307614730e226
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-29 17:32:43 +0900

    feat: 페이지네이션 기본값 설정 (#47)

--- src/main/java/com/smunity/server/domain/question/controller/QuestionController.java
+++ src/main/java/com/smunity/server/domain/question/controller/QuestionController.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +25,7 @@ public class QuestionController {
     private final QuestionCommandService questionCommandService;
 
     @GetMapping
-    public ResponseEntity<Page<QuestionResponseDto>> getQuestions(@ParameterObject Pageable pageable) {
+    public ResponseEntity<Page<QuestionResponseDto>> getQuestions(@ParameterObject @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<QuestionResponseDto> responseDtoPage = questionQueryService.getQuestions(pageable);
         return ResponseEntity.ok(responseDtoPage);
     }
