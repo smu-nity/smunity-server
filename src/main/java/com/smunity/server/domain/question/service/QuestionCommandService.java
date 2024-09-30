935bfbf080e7db2362b54893d92d52e4dbd670c9
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-30 20:58:34 +0900

    refactor: 동일 로직 util로 리팩토링 (#50)

--- src/main/java/com/smunity/server/domain/question/service/QuestionCommandService.java
+++ src/main/java/com/smunity/server/domain/question/service/QuestionCommandService.java
@@ -5,10 +5,10 @@ import com.smunity.server.domain.question.dto.QuestionResponseDto;
 import com.smunity.server.domain.question.entity.Question;
 import com.smunity.server.domain.question.repository.QuestionRepository;
 import com.smunity.server.global.common.entity.Member;
-import com.smunity.server.global.common.entity.enums.MemberRole;
 import com.smunity.server.global.common.repository.MemberRepository;
 import com.smunity.server.global.exception.GeneralException;
 import com.smunity.server.global.exception.code.ErrorCode;
+import com.smunity.server.global.security.util.PermissionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,19 +29,11 @@ public class QuestionCommandService {
         return QuestionResponseDto.from(questionRepository.save(question));
     }
 
-    public QuestionResponseDto updateQuestion(Long memberId, Long questionId, QuestionRequestDto requestDto) {
+    public QuestionResponseDto updateQuestion(Long memberId, Boolean isAdmin, Long questionId, QuestionRequestDto requestDto) {
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new GeneralException(ErrorCode.QUESTION_NOT_FOUND));
-        validateAccess(memberId, question.getMember().getId());
+        PermissionUtil.validatePermission(memberId, isAdmin, question.getMember().getId());
         question.update(requestDto.title(), requestDto.content(), requestDto.anonymous());
         return QuestionResponseDto.from(question);
-    }
-
-    private void validateAccess(Long memberId, Long authorId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new GeneralException(ErrorCode.MEMBER_NOT_FOUND));
-        if (!memberId.equals(authorId) && !member.getRole().equals(MemberRole.ROLE_ADMIN)) {
-            throw new GeneralException(ErrorCode.QUESTION_FORBIDDEN);
-        }
     }
 }
--- src/main/java/com/smunity/server/global/security/annotation/resolver/AuthAdminArgumentResolver.java
+++ src/main/java/com/smunity/server/global/security/annotation/resolver/AuthAdminArgumentResolver.java
@@ -2,20 +2,16 @@ package com.smunity.server.global.security.annotation.resolver;
 
 import com.smunity.server.global.security.annotation.AuthAdmin;
 import com.smunity.server.global.security.provider.JwtTokenProvider;
+import com.smunity.server.global.security.util.PermissionUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import java.util.Collection;
-
-import static com.smunity.server.global.common.entity.enums.MemberRole.ROLE_ADMIN;
 
 /**
  * 컨트롤러 메서드의 파라미터가 @AuthAdmin Boolean 타입일 때 해당 파라미터를 처리하도록 지정하는 클래스
@@ -45,13 +41,6 @@ public class AuthAdminArgumentResolver implements HandlerMethodArgumentResolver 
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         String jwt = jwtTokenProvider.resolveToken(request);
         return jwtTokenProvider.validateToken(jwt, false) ?
-                isAdmin(jwtTokenProvider.getAuthentication(jwt).getAuthorities()) : null;
-    }
-
-    // 주어진 권한 목록에 관리자 권한이 포함되어 있는지 확인
-    private boolean isAdmin(Collection<? extends GrantedAuthority> authorities) {
-        return authorities.stream()
-                .map(GrantedAuthority::getAuthority)
-                .anyMatch(ROLE_ADMIN.name()::equals);
+                PermissionUtil.isAdmin(jwtTokenProvider.getAuthentication(jwt).getAuthorities()) : null;
     }
 }
