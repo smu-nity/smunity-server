f8eee1aaa9d1c02d1f6abeaa9c7c30091fc54d8e
hyunmin0317 <hyunmin-choi@naver.com>
2024-09-30 21:50:51 +0900

    feat: 답변 생성 API (#53)

---
+++ src/main/java/com/smunity/server/domain/answer/controller/AnswerController.java
@@ -0,0 +1,25 @@
+package com.smunity.server.domain.answer.controller;
+
+import com.smunity.server.domain.answer.dto.AnswerRequestDto;
+import com.smunity.server.domain.answer.dto.AnswerResponseDto;
+import com.smunity.server.domain.answer.service.AnswerCommandService;
+import com.smunity.server.global.security.annotation.AuthMember;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/questions/{questionId}/answer")
+public class AnswerController {
+
+    private final AnswerCommandService answerCommandService;
+
+    @PostMapping
+    public ResponseEntity<AnswerResponseDto> createAnswer(@AuthMember Long memberId, @PathVariable Long questionId, @Valid @RequestBody AnswerRequestDto requestDto) {
+        AnswerResponseDto responseDto = answerCommandService.createAnswer(memberId, questionId, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+}
