7f23c1510267638270e152c9f9bddb5c3c1e3a7f
Hyunmin Choi <hyunmin-choi@naver.com>
2024-10-01 10:58:20 +0900

    [FEATURE] 답변 조회 API (#62)
    * feat: 답변 조회 관련 에러 코드 (#52)
    
    * feat: 답변 조회 메서드 (#52)
    
    * feat: 답변 조회 API (#52)

--- src/main/java/com/smunity/server/domain/answer/controller/AnswerController.java
+++ src/main/java/com/smunity/server/domain/answer/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.smunity.server.domain.answer.controller;
 import com.smunity.server.domain.answer.dto.AnswerRequestDto;
 import com.smunity.server.domain.answer.dto.AnswerResponseDto;
 import com.smunity.server.domain.answer.service.AnswerCommandService;
+import com.smunity.server.domain.answer.service.AnswerQueryService;
 import com.smunity.server.global.security.annotation.AuthMember;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/questions/{questionId}/answer")
 public class AnswerController {
 
+    private final AnswerQueryService answerQueryService;
     private final AnswerCommandService answerCommandService;
+
+    @GetMapping
+    public ResponseEntity<AnswerResponseDto> getAnswer(@PathVariable Long questionId) {
+        AnswerResponseDto answer = answerQueryService.getAnswer(questionId);
+        return ResponseEntity.ok(answer);
+    }
 
     @PostMapping
     public ResponseEntity<AnswerResponseDto> createAnswer(@AuthMember Long memberId, @PathVariable Long questionId, @Valid @RequestBody AnswerRequestDto requestDto) {
--- src/main/java/com/smunity/server/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/smunity/server/domain/answer/repository/AnswerRepository.java
@@ -3,6 +3,9 @@ package com.smunity.server.domain.answer.repository;
 import com.smunity.server.domain.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
+    Optional<Answer> findByQuestionId(Long questionId);
 }
