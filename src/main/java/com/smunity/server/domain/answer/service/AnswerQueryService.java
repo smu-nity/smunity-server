1068764602ec152ae3288ff932fa11d79d06a9ec
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-01 10:57:19 +0900

    feat: 답변 조회 메서드 (#52)

---
+++ src/main/java/com/smunity/server/domain/answer/service/AnswerQueryService.java
@@ -0,0 +1,24 @@
+package com.smunity.server.domain.answer.service;
+
+import com.smunity.server.domain.answer.dto.AnswerResponseDto;
+import com.smunity.server.domain.answer.entity.Answer;
+import com.smunity.server.domain.answer.repository.AnswerRepository;
+import com.smunity.server.global.exception.GeneralException;
+import com.smunity.server.global.exception.code.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AnswerQueryService {
+
+    private final AnswerRepository answerRepository;
+
+    public AnswerResponseDto getAnswer(Long questionId) {
+        Answer answer = answerRepository.findByQuestionId(questionId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.ANSWER_NOT_FOUND));
+        return AnswerResponseDto.from(answer);
+    }
+}
