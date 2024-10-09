b5960b11e19f8b9ea009337b85a8f176ff8c2e59
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-09 16:36:41 +0900

    fix: 권한 확인 메서드 (#82)

--- src/main/java/com/smunity/server/global/security/util/PermissionUtil.java
+++ src/main/java/com/smunity/server/global/security/util/PermissionUtil.java
@@ -12,14 +12,14 @@ public class PermissionUtil {
 
     // 주어진 회원 ID(memberId), 관리자인지 여부(isAdmin), 작성자 ID(authorId)를 기준으로 권한 검증
     public static void validatePermission(Long memberId, Boolean isAdmin, Long authorId) {
-        if (checkPermission(memberId, isAdmin, authorId)) {
+        if (!checkPermission(memberId, isAdmin, authorId)) {
             throw new GeneralException(ErrorCode.MEMBER_FORBIDDEN);
         }
     }
 
     // 회원 ID(memberId)와 작성자 ID(authorId)를 비교하고 관리자인지 여부(isAdmin)를 검사하여 권한 확인
     public static boolean checkPermission(Long memberId, boolean isAdmin, Long authorId) {
-        return !memberId.equals(authorId) && !isAdmin;
+        return memberId.equals(authorId) || isAdmin;
     }
 
     // 주어진 권한 목록에 관리자 권한이 포함되어 있는지 확인
