de1b88c5d3e43766ffbdf5b8617d597ae894de37
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-16 20:39:00 +0900

    feat: 질문 조회 API 스펙 변경 (#86)

--- src/main/java/com/smunity/server/domain/question/dto/QuestionReadResponseDto.java
+++ src/main/java/com/smunity/server/domain/question/dto/QuestionReadResponseDto.java
@@ -2,29 +2,36 @@ package com.smunity.server.domain.question.dto;
 
 import com.smunity.server.domain.question.entity.Question;
 import lombok.Builder;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 
 @Builder
-public record QuestionResponseDto(
+public record QuestionReadResponseDto(
         Long id,
         String title,
         String content,
         String author,
+        boolean isAuthor,
         boolean answered,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
 
-    public static QuestionResponseDto from(Question question) {
-        return QuestionResponseDto.builder()
+    public static QuestionReadResponseDto of(Long memberId, Question question) {
+        return QuestionReadResponseDto.builder()
                 .id(question.getId())
                 .title(question.getTitle())
                 .content(question.getContent())
                 .author(question.getAuthor())
+                .isAuthor(question.getIsAuthor(memberId))
                 .answered(question.getAnswered())
                 .createdAt(question.getCreatedAt())
                 .updatedAt(question.getUpdatedAt())
                 .build();
+    }
+
+    public static Page<QuestionReadResponseDto> of(Long memberId, Page<Question> questions) {
+        return questions.map(question -> of(memberId, question));
     }
 }
