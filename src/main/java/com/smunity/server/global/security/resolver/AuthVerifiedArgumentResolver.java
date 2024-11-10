61cf0728f053a1ee10f732ca6e96363b579714a6
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-10 14:15:39 +0900

    feat: AuthVerified 어노테이션 (#93)

---
+++ src/main/java/com/smunity/server/global/security/resolver/AuthVerifiedArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.smunity.server.global.security.resolver;
+
+import com.smunity.server.global.security.annotation.AuthVerified;
+import com.smunity.server.global.security.provider.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * 컨트롤러 메서드의 파라미터가 @AuthVerified String 타입일 때 해당 파라미터를 처리하도록 지정하는 클래스
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthVerifiedArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    /**
+     * 파라미터 타입 확인 (@AuthVerified, String)
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAuthMemberAnnotation = parameter.hasParameterAnnotation(AuthVerified.class);
+        boolean isStringType = String.class.isAssignableFrom(parameter.getParameterType());
+        return hasAuthMemberAnnotation && isStringType;
+    }
+
+    /**
+     * 해당 컨트롤러 메서드의 파라미터 처리
+     */
+    @Override
+    public String resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  @NonNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String jwt = jwtTokenProvider.resolveToken(request);
+        return jwtTokenProvider.getUsername(jwt);
+    }
+}
