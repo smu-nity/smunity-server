2bf65795fbb1ab46bef502a810deeb0abc85b6ba
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-12 21:00:51 +0900

    feat: 사용자 인증 API 분리 (#98)

--- src/main/java/com/smunity/server/domain/auth/service/AuthService.java
+++ src/main/java/com/smunity/server/domain/auth/service/AuthService.java
@@ -4,19 +4,30 @@ import com.smunity.server.domain.auth.dto.AuthCourseResponseDto;
 import com.smunity.server.domain.auth.dto.AuthRequestDto;
 import com.smunity.server.domain.auth.dto.AuthResponseDto;
 import com.smunity.server.domain.auth.util.AuthUtil;
+import com.smunity.server.global.common.repository.MemberRepository;
+import com.smunity.server.global.exception.GeneralException;
+import com.smunity.server.global.exception.code.ErrorCode;
 import com.smunity.server.global.security.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
+
+    public List<AuthCourseResponseDto> readCourses(AuthRequestDto requestDto) {
+        JSONArray response = AuthUtil.getCourses(requestDto);
+        return AuthCourseResponseDto.from(response);
+    }
 
     public AuthResponseDto authenticate(AuthRequestDto requestDto) {
         JSONObject response = AuthUtil.getInfo(requestDto);
@@ -24,8 +35,25 @@ public class AuthService {
         return AuthResponseDto.of(response, authToken);
     }
 
-    public List<AuthCourseResponseDto> readCourses(AuthRequestDto requestDto) {
-        JSONArray response = AuthUtil.getCourses(requestDto);
-        return AuthCourseResponseDto.from(response);
+    public AuthResponseDto registerAuth(AuthRequestDto requestDto) {
+        validateUsername(requestDto.username());
+        return authenticate(requestDto);
+    }
+
+    public AuthResponseDto resetPassword(AuthRequestDto requestDto) {
+        validateExistingUsername(requestDto.username());
+        return authenticate(requestDto);
+    }
+
+    private void validateUsername(String username) {
+        if (memberRepository.existsByUsername(username)) {
+            throw new GeneralException(ErrorCode.ACCOUNT_CONFLICT);
+        }
+    }
+
+    private void validateExistingUsername(String username) {
+        if (!memberRepository.existsByUsername(username)) {
+            throw new GeneralException(ErrorCode.MEMBER_NOT_FOUND);
+        }
     }
 }
