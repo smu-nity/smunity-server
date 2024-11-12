08f509d30be12714cc5a4efc0cb2ee3d708a249a
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-12 21:08:10 +0900

    refactor: 메서드 순서 변경 (#98)

--- src/main/java/com/smunity/server/domain/auth/util/AuthUtil.java
+++ src/main/java/com/smunity/server/domain/auth/util/AuthUtil.java
@@ -20,13 +20,13 @@ public class AuthUtil {
     private static final String LOGIN_URL = "https://smsso.smu.ac.kr/Login.do";
     private static final String BASE_URL = "https://smul.smu.ac.kr";
 
+    public static JSONArray getCourses(AuthRequestDto requestDto) {
+        return getData(requestDto, "/UsrRecMatt/list.do", "dsRecMattList");
+    }
+
     public static JSONObject getInfo(AuthRequestDto requestDto) {
         JSONArray response = getData(requestDto, "/UsrSchMng/selectStdInfo.do", "dsStdInfoList");
         return response.getJSONObject(0);
-    }
-
-    public static JSONArray getCourses(AuthRequestDto requestDto) {
-        return getData(requestDto, "/UsrRecMatt/list.do", "dsRecMattList");
     }
 
     private static JSONArray getData(AuthRequestDto requestDto, String url, String key) {
