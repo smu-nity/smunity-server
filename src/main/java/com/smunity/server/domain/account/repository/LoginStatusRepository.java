e0bf07d829b05ad46f0044f38521fd5c5cb2e520
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-16 20:59:27 +0900

    feat: 로그인 정보 리포지토리 (#116)

---
+++ src/main/java/com/smunity/server/domain/account/repository/LoginStatusRepository.java
@@ -0,0 +1,8 @@
+package com.smunity.server.domain.account.repository;
+
+import com.smunity.server.domain.account.entity.LoginStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginStatusRepository extends JpaRepository<LoginStatus, Long> {
+
+}
