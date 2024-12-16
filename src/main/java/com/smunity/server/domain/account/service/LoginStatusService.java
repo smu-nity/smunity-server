415f058df90c2e980c5775dc0ab143dce9e0876b
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-16 20:59:40 +0900

    feat: 로그인 정보 서비스 (#116)

---
+++ src/main/java/com/smunity/server/domain/account/service/LoginStatusService.java
@@ -0,0 +1,32 @@
+package com.smunity.server.domain.account.service;
+
+import com.smunity.server.domain.account.dto.LoginRequestDto;
+import com.smunity.server.domain.account.entity.LoginStatus;
+import com.smunity.server.domain.account.repository.LoginStatusRepository;
+import com.smunity.server.domain.account.util.IpUtil;
+import com.smunity.server.global.common.entity.Member;
+import com.smunity.server.global.common.repository.MemberRepository;
+import com.smunity.server.global.exception.GeneralException;
+import com.smunity.server.global.exception.code.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LoginStatusService {
+
+    private final MemberRepository memberRepository;
+    private final LoginStatusRepository loginStatusRepository;
+    private final HttpServletRequest request;
+
+    public void createLoginStatus(LoginRequestDto requestDto) {
+        Member member = memberRepository.findByUsername(requestDto.username())
+                .orElseThrow(() -> new GeneralException(ErrorCode.ACCOUNT_NOT_FOUND));
+        LoginStatus loginStatus = requestDto.toEntity(IpUtil.getClientIp(request));
+        loginStatus.setMember(member);
+        loginStatusRepository.save(loginStatus);
+    }
+}
