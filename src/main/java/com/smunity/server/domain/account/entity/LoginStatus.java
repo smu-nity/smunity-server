b9e143628331babf0d8769b819d685a8aea1230b
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-16 20:59:14 +0900

    feat: 로그인 정보 엔티티 (#116)

---
+++ src/main/java/com/smunity/server/domain/account/entity/LoginStatus.java
@@ -0,0 +1,38 @@
+package com.smunity.server.domain.account.entity;
+
+import com.smunity.server.global.common.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "account_login_status")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginStatus {
+
+    @Id
+    @Column(name = "login_status_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime loginAt;
+
+    private String ipAddress;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
