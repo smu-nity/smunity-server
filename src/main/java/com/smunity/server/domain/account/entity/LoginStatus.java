9fda515e956a2dc1f6dd9c0053a086c0a1120585
hyunmin0317 <hyunmin-choi@naver.com>
2024-12-16 21:05:34 +0900

    refactor: 엔티티 리팩토링 (#116)

--- src/main/java/com/smunity/server/domain/account/entity/LoginStatus.java
+++ src/main/java/com/smunity/server/domain/account/entity/LoginStatus.java
@@ -3,28 +3,19 @@ package com.smunity.server.domain.account.entity;
 import com.smunity.server.global.common.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Builder
 @Table(name = "account_login_status")
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class LoginStatus {
+public class LoginStatus extends TimeEntity {
 
     @Id
     @Column(name = "login_status_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime loginAt;
 
     private String ipAddress;
 
