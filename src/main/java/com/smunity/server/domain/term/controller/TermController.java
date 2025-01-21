2aeb9990dc620bb3f420c5bfc26b209a40d4ecad
Hyunmin Choi <hyunmin-choi@naver.com>
2025-01-21 20:14:04 +0900

    [FEATURE] 현재 학기 API (#164)
    * refactor: 패키지 변경 (#163)
    
    * feat: 학기 엔티티 (#163)
    
    * feat: 학기 리포지토리 (#163)
    
    * feat: 현재 학기 API (#163)
    
    * feat: 현재 학기 API 권한 설정 (#163)
    
    * chore: API 설명 추가 (#163)

---
+++ src/main/java/com/smunity/server/domain/term/controller/TermController.java
@@ -0,0 +1,27 @@
+package com.smunity.server.domain.term.controller;
+
+import com.smunity.server.domain.term.dto.TermResponseDto;
+import com.smunity.server.domain.term.service.TermService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/terms")
+@Tag(name = "10 Term API", description = "학기 관련 API")
+public class TermController {
+
+    private final TermService termService;
+
+    @GetMapping("/current")
+    @Operation(summary = "현재 학기 조회", description = "현재 학기를 조회합니다.")
+    public ResponseEntity<TermResponseDto> readCurrentTerm() {
+        TermResponseDto responseDto = termService.readCurrentTerm();
+        return ResponseEntity.ok(responseDto);
+    }
+}
