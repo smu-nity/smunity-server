3fb4f6afa520a50cc172da8e8e9e916edb0b6466
hyunmin0317 <hyunmin-choi@naver.com>
2025-04-06 20:24:14 +0900

    fix: 회원가입 학번 조회 로직 수정 (#215)

--- src/main/java/com/smunity/server/global/common/repository/YearRepository.java
+++ src/main/java/com/smunity/server/global/common/repository/YearRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface YearRepository extends JpaRepository<Year, Long> {
 
-    Optional<Year> findByName(String name);
+    Optional<Year> findByValue(Integer value);
 }
