16bb07f5a94881f8802791de679ddfd1eddc5c91
hyunmin0317 <hyunmin-choi@naver.com>
2025-05-22 20:36:19 +0900

    refactor: major 패키지 (#255)

--- src/main/java/com/smunity/server/domain/major/mapper/MajorMapper.java
+++ src/main/java/com/smunity/server/domain/major/mapper/MajorMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface MajorMapper {
 
     MajorMapper INSTANCE = Mappers.getMapper(MajorMapper.class);
