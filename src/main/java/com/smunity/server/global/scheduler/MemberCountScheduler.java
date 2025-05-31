17d49db182d92e4817409265e9634bdf16fe67b1
Hyunmin Choi <hyunmin-choi@naver.com>
2025-05-31 09:01:55 +0900

    [FEATURE] 학과별 사용자 수 통계 처리 로직 변경 (#265)
    * feat: 학과별 사용자 수 통계 처리 메서드 (#263)
    
    * feat: 학과별 사용자 수 통계 처리 스케줄러 (#263)
    
    * chore: 에러 로그 형식 변경 (#263)
    
    * chore: 에러 로그 형식 변경 (#263)
    
    * chore: 스케줄러 로그 형식 변경 (#263)
    
    * delete: 사용자 생성/삭제 로직에서 학과 사용자 수 계산 제거 (#263)

---
+++ src/main/java/com/smunity/server/global/scheduler/MemberCountScheduler.java
@@ -0,0 +1,23 @@
+package com.smunity.server.global.scheduler;
+
+import com.smunity.server.domain.department.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MemberCountScheduler {
+
+    private final DepartmentService departmentService;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void runUpdateMemberCount() {
+        departmentService.updateMemberCount();
+        log.info("Member count update completed : {}", LocalDateTime.now());
+    }
+}
