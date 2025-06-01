8e427605dcf398740382b0d6220f60adc2801f7e
hyunmin0317 <hyunmin-choi@naver.com>
2025-06-01 09:10:06 +0900

    chore: 에러 메시지 수정 (#264)

--- src/main/java/com/smunity/server/global/exception/DepartmentNotFoundException.java
+++ src/main/java/com/smunity/server/global/exception/DepartmentNotFoundException.java
@@ -3,6 +3,6 @@ package com.smunity.server.global.exception;
 public class DepartmentNotFoundException extends RuntimeException {
 
     public DepartmentNotFoundException(String name) {
-        super(String.format("Department '%s' not found.", name));
+        super(String.format("Department '%s' not found", name));
     }
 }
