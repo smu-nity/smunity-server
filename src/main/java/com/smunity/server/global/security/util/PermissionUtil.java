b62cd596da9e596be3f5a5a64552af8e45b7a2d8
hyunmin0317 <hyunmin-choi@naver.com>
2025-08-28 15:42:46 +0900

    refactor: 코드 리팩토링 (#279)

--- src/main/java/com/smunity/server/global/security/util/PermissionUtil.java
+++ src/main/java/com/smunity/server/global/security/util/PermissionUtil.java
@@ -17,15 +17,15 @@ public class PermissionUtil {
         }
     }
 
-    // 회원 ID(memberId)와 작성자 ID(authorId)를 비교하고 관리자인지 여부(isAdmin)를 검사하여 권한 확인
-    public static boolean checkPermission(Long memberId, boolean isAdmin, Long authorId) {
-        return memberId.equals(authorId) || isAdmin;
-    }
-
     // 주어진 권한 목록에 관리자 권한이 포함되어 있는지 확인
     public static boolean isAdmin(Collection<? extends GrantedAuthority> authorities) {
         return authorities.stream()
                 .map(GrantedAuthority::getAuthority)
                 .anyMatch(ROLE_ADMIN.name()::equals);
+    }
+
+    // 회원 ID(memberId)와 작성자 ID(authorId)를 비교하고 관리자인지 여부(isAdmin)를 검사하여 권한 확인
+    private static boolean checkPermission(Long memberId, boolean isAdmin, Long authorId) {
+        return memberId.equals(authorId) || isAdmin;
     }
 }
